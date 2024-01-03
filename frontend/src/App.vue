e7db317c8afe61cecea8d9aab50b52457ed53f23
semihsezer <semih.c.sezer@gmail.com>
2024-01-03 12:08:56 +0000

    Escape removes focus out of the input field

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,12 +7,15 @@
 
   <main>
     <div class="card flex justify-content-center">
-        <AutoComplete id="myAutoComplete" v-model="selectedValue" :suggestions="items" optionLabel="title" @complete="search" @item-select="handleUpdate" placeholder="Search Tab Groups..." completeOnFocus="true" delay="100" panelClass="autofocusPanel" style="width: 100%"/>
+        <AutoComplete id="autoComplete" ref="autoCompleteElement" v-model="selectedValue" :suggestions="items" optionLabel="title" @complete="search" @item-select="handleUpdate" @hide="handleHide(isFocus)" placeholder="Search Tab Groups..." completeOnFocus="true" delay="100" panelClass="autofocusPanel" style="width: 100%" />
     </div>
   </main>
 </template>
 
 <script>
+
+import { ref } from "vue";
+const autoCompleteElement = ref();
 
 function focusToTab(windowId, tabId, callback) {
   if (tabId) {
@@ -43,22 +46,22 @@ export default {
         },
         handleUpdate() {
           var windowId = this.selectedValue.windowId;
-          //console.log(`handleUpdate: ${this.selectedValue}`);
           focusToTab(windowId, null);
-        }
+        },
+
+        handleHide(isFocus) {
+          this.$refs.autoCompleteElement.$refs.focusInput.blur();
+        },
     },
     mounted() {
       chrome.tabGroups.query({},  function (groups) {
         this.allItems = groups.sort((a, b) => a.title.localeCompare(b.title));
         console.log(this.allItems);
         this.items = this.allItems;
-        myAutoComplete.firstElementChild.focus();
       });
-    },
-    watch: {
-      selectedValue(value) {
-        console.log(value);
-      }
+      this.$nextTick(() => {
+        this.$refs.autoCompleteElement.$refs.focusInput.focus();
+      });
     }
 };
 </script>
