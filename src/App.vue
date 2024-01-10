6f8e38e5733e8e2f9b007f17e97c058257dc8f2c
semihsezer <semih.c.sezer@gmail.com>
2024-01-10 22:48:55 +0000

    Override escape key to remove focus

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 
   <main>
     <div class="card flex justify-content-center">
-        <AutoComplete id="autoComplete" ref="autoCompleteElement" v-model="selectedValue" :suggestions="items" optionLabel="title" @complete="search" @item-select="handleUpdate" @hide="handleHide(isFocus)" placeholder="Search Tab Groups..." completeOnFocus="true" delay="100" panelClass="autofocusPanel" style="width: 100%" />
+        <AutoComplete id="autoComplete" ref="autoCompleteElement" v-model="selectedValue" :suggestions="items" optionLabel="title" @complete="search" @item-select="handleUpdate" placeholder="Search Tab Groups..." completeOnFocus="true" delay="100" panelClass="autofocusPanel" style="width: 100%" />
     </div>
   </main>
 </template>
@@ -47,17 +47,16 @@ export default {
         handleUpdate() {
           var windowId = this.selectedValue.windowId;
           focusToTab(windowId, null);
-        },
-
-        handleHide(isFocus) {
-          this.$refs.autoCompleteElement.$refs.focusInput.blur();
-        },
+        }
     },
     mounted() {
       chrome.tabGroups.query({},  function (groups) {
         this.allItems = groups.sort((a, b) => a.title.localeCompare(b.title));
         this.items = this.allItems;
       });
+      this.$refs.autoCompleteElement.onEscapeKey = function(){
+        this.$refs.focusInput.blur();
+      }
       this.$nextTick(() => {
         this.$refs.autoCompleteElement.$refs.focusInput.focus();
       });
